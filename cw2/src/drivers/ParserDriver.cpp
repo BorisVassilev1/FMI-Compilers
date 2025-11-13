ce513dfe003f29ce310573d300700c5a8d44fa16
Boris Vassilev <boris.n.vassilev@gmail.com>
2025-11-14 00:52:34 +0200

    edited grammar

--- cw2/src/drivers/ParserDriver.cpp
+++ cw2/src/drivers/ParserDriver.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <iostream>
 #include <fstream>
+#include "DPDA/cfg.h"
 #include "DPDA/token.h"
 
 #define DBG_LOG_LEVEL 2
@@ -9,22 +10,30 @@
 
 #include <lang/grammar_factory.hpp>
 
-CREATE_TOKEN_CONSTEXPR(program, "_program");
-CREATE_TOKEN_CONSTEXPR(class_, "_class");
-CREATE_TOKEN_CONSTEXPR(feature, "_feature");
-CREATE_TOKEN_CONSTEXPR(formal, "_formal");
-CREATE_TOKEN_CONSTEXPR(expr, "_expr");
+CREATE_TOKEN_CONSTEXPR(program, "_program")
+CREATE_TOKEN_CONSTEXPR(class_, "_class")
+CREATE_TOKEN_CONSTEXPR(feature, "_feature")
+CREATE_TOKEN_CONSTEXPR(formal, "_formal")
+CREATE_TOKEN_CONSTEXPR(expr, "_expr")
 
-CREATE_TOKEN_CONSTEXPR(literal, "_literal");
-CREATE_TOKEN_CONSTEXPR(memberexpr, "_memberexpr");
-CREATE_TOKEN_CONSTEXPR(neighexpr, "_neighexpr");
-CREATE_TOKEN_CONSTEXPR(isvoidexpr, "_isvoidexpr");
-CREATE_TOKEN_CONSTEXPR(mulexpr, "_mulexpr");
-CREATE_TOKEN_CONSTEXPR(arithexpr, "_arithexpr");
-CREATE_TOKEN_CONSTEXPR(compexpr, "_compexpr");
-CREATE_TOKEN_CONSTEXPR(notexpr, "_notexpr");
-CREATE_TOKEN_CONSTEXPR(assignexpr, "_assignexpr");
-CREATE_TOKEN_CONSTEXPR(condexpr, "_condexpr");
+CREATE_TOKEN_CONSTEXPR(literal, "_literal")
+CREATE_TOKEN_CONSTEXPR(memberexpr, "_memberexpr")
+CREATE_TOKEN_CONSTEXPR(neighexpr, "_neighexpr")
+CREATE_TOKEN_CONSTEXPR(isvoidexpr, "_isvoidexpr")
+CREATE_TOKEN_CONSTEXPR(mulexpr, "_mulexpr")
+CREATE_TOKEN_CONSTEXPR(arithexpr, "_arithexpr")
+CREATE_TOKEN_CONSTEXPR(compexpr, "_compexpr")
+CREATE_TOKEN_CONSTEXPR(notexpr, "_notexpr")
+CREATE_TOKEN_CONSTEXPR(assignexpr, "_assignexpr")
+CREATE_TOKEN_CONSTEXPR(condexpr, "_condexpr")
+CREATE_TOKEN_CONSTEXPR(ID_expr, "ID_expr")
+CREATE_TOKEN_CONSTEXPR(NONID_expr, "nonID_expr")
+CREATE_TOKEN_CONSTEXPR(init, "init")
+CREATE_TOKEN_CONSTEXPR(caseLine, "caseLine")
+CREATE_TOKEN_CONSTEXPR(finish_expr, "finish_expr")
+CREATE_TOKEN_CONSTEXPR(finish_aexpr, "finish_aexpr")
+CREATE_TOKEN_CONSTEXPR(term, "term")
+CREATE_TOKEN_CONSTEXPR(finish_term, "finish_term")
 
 auto createParser() {
 	using namespace ll1g;
@@ -39,133 +48,138 @@ auto createParser() {
 	auto Program = Repeat<Token>(program, Production<Token>({class_}, {false}), -1);
 
 	current = class_;
-	auto Class = Seq<Token>(class_, 
+	auto Class = Seq<Token>(class_,
 		CLASS,
-		TYPEID, 
+		TYPEID,
 		Optional<Token>(NT(), Word<Token>(NT(), {INHERITS, TYPEID})),
-		Token('{'), 
-		Repeat<Token>(NT(), feature, -1), 
-		Token('}'), 
+		Token('{'),
+		Repeat<Token>(NT(), feature, -1),
+		Token('}'),
 		Token(';')
 	);
 
+	current = init;
+	auto Init = Seq<Token>(NT(),
+		OBJECTID, Token(':'), TYPEID, Optional<Token>(NT(), Production<Token>({ASSIGN, expr})));
+
 	current = feature;
-	auto Feature = Seq<Token>(feature, 
+	auto Feature = Seq<Token>(feature,
 		OBJECTID,
 		Choice<Token>(NT(),
-			Seq<Token>(NT(), 
-				Token('('), formal, 
-					Repeat<Token>(NT(), Seq<Token>(NT(), Token(','), formal), -1), 
+			Seq<Token>(NT(),
+				Token('('), formal,
+					Repeat<Token>(NT(), Seq<Token>(NT(), Token(','), formal), -1),
 				Token(')'), Token(':'), TYPEID, Token('{'), expr, Token('}')
 			),
-			Seq<Token>(NT(), Token(':'), TYPEID, Optional<Token>(NT(), Seq<Token>(NT(), Token('<'), Token('-'), expr)))
+			Seq<Token>(NT(), Token(':'), TYPEID, Optional<Token>(NT(), Production<Token>({ASSIGN, expr})))
 		)
 	);
 
 	current = formal;
-	auto Formal = Seq<Token>(formal, 
+	auto Formal = Seq<Token>(formal,
 		OBJECTID, Token(':'), TYPEID
 	);
 
-	current = literal;
-	auto Literal = Choice<Token>(literal,
+	current = caseLine;
+	auto CaseLine = Seq<Token>(caseLine,
+		OBJECTID, Token(':'), TYPEID, DARROW, expr, Token(';')
+	);
+
+	current = NONID_expr;
+	auto NONID_Expr = Choice<Token>(NONID_expr,
+		Production<Token>({IF, expr, THEN, expr, ELSE, expr, FI}),
+		Production<Token>({WHILE, expr, LOOP, expr, POOL}),
+		Seq<Token>(NT(),
+			Token('{'),
+			expr,
+			Token(';'),
+			Repeat<Token>(NT(), Production<Token>({expr, Token(';')})),
+			Token('}')
+		),
+		Seq<Token>(NT(),
+			LET, init, Repeat<Token>(NT(), Production<Token>({Token(','), init})), IN, expr
+		),
+		Seq<Token>(NT(),
+			CASE, expr, OF, caseLine, Repeat<Token>(NT(), caseLine), ESAC
+		),
+		Production<Token>({NEW, TYPEID}),
 		INT_CONST,
 		STR_CONST,
-		BOOL_CONST_TRUE,
-		BOOL_CONST_FALSE
+		BOOL_CONST_FALSE,
+		BOOL_CONST_TRUE
+	);
+
+	current = assignexpr;
+	auto AssignExpr = Choice<Token>(assignexpr,
+		Seq<Token>(NT(),
+			OBJECTID,
+			Choice<Token>(NT(),
+				Production<Token>({ASSIGN, assignexpr}),
+				finish_expr
+			)
+		),
+		NONID_expr
+	);
+
+	current = finish_expr;
+	auto FinishExpr = Choice<Token>(finish_expr,
+		Production<Token>({LE, arithexpr}),
+		Production<Token>({Token('<'), arithexpr}),
+		Production<Token>({Token('='), arithexpr}),
+		finish_aexpr
 	);
 
 	current = expr;
 	auto Expr = Choice<Token>(expr,
-		Seq<Token>(NT(), IF, expr, THEN, expr, ELSE, expr, FI),
-		Seq<Token>(NT(), WHILE, expr, LOOP, expr, POOL),
-		Seq<Token>(NT(), LET, formal, Repeat<Token>(NT(), Production<Token>({Token(','), formal})), IN, expr),
-		Seq<Token>(NT(), Token('{'), expr, Repeat<Token>(NT(), Production<Token>({Token(';'), expr})), Token('}')),
+		Seq<Token>(NT(), arithexpr, finish_expr),
+		NONID_Expr
+	);
+
+	current = finish_aexpr;
+	auto FinishAExpr = Choice<Token>(finish_aexpr,
 		Seq<Token>(NT(),
-			CASE, expr, Token(':'), OF, OBJECTID, Token(':'), TYPEID, DARROW, expr,
-			Repeat<Token>(NT(), Production<Token>({Token(';'), OBJECTID, Token(':'), TYPEID, DARROW, expr})),
-			ESAC
+			Choice<Token>(NT(),
+				Production<Token>({Token('+'), term}),
+				Production<Token>({Token('-'), term})
+			),
+			RepeatChoice<Token>(NT(),
+				Production<Token>({Token('+'), term}),
+				Production<Token>({Token('-'), term})
+			)
 		),
-		Production<Token>({NEW, TYPEID}),
-		assignexpr,
-		literal
-	);
-
-	current = assignexpr;
-	auto AssignExpr = Seq<Token>(assignexpr, 
-		OBJECTID, 
-		ASSIGN,
-		Choice<Token>(NT(),
-			assignexpr,
-			notexpr
-		)
-	);
-
-	current = notexpr;
-	auto NotExpr = Choice<Token>(notexpr,
-		Seq<Token>(NT(), NOT, notexpr),
-		compexpr
-	);
-
-	current = compexpr;
-	auto CompExpr = Choice<Token>(compexpr,
-		Production<Token>({arithexpr, LE, arithexpr}),
-		Seq<Token>(NT(), arithexpr, Token('='), arithexpr),
-		Seq<Token>(NT(), arithexpr, Token('<'), arithexpr),
-		arithexpr
+		finish_term
 	);
 
 	current = arithexpr;
-	auto ArithExpr = Seq<Token>(arithexpr,
-		mulexpr,
-		RepeatChoice<Token>(NT(),
-				Seq<Token>(NT(), Token('+'), mulexpr),
-				Seq<Token>(NT(), Token('-'), mulexpr)
-			)
-		);
+	auto ArithExpr = Seq<Token>(arithexpr, term, finish_aexpr);
 
-	current = mulexpr;
-	auto MulExpr = Seq<Token>(mulexpr,
-		isvoidexpr,
-		RepeatChoice<Token>(NT(),
-				Seq<Token>(NT(), Token('*'), isvoidexpr),
-				Seq<Token>(NT(), Token('/'), isvoidexpr)
-			)
-		);
+	current = finish_term;
+	auto FinishTerm = RepeatChoice(finish_term,
+		Production<Token>({Token('*'), isvoidexpr}),
+		Production<Token>({Token('/'), isvoidexpr})
+	);
+
+	current = term;
+	auto Term = Seq<Token>(term, isvoidexpr, finish_term);
 
 	current = isvoidexpr;
 	auto IsVoidExpr = Choice<Token>(isvoidexpr,
-		Seq<Token>(NT(), ISVOID, isvoidexpr),
+		Production<Token>({ISVOID, neighexpr}),
 		neighexpr
 	);
 
 	current = neighexpr;
 	auto NeighExpr = Choice<Token>(neighexpr,
-		Seq<Token>(NT(), Token('~'), neighexpr),
-		memberexpr
-	);
-
-	current = memberexpr;
-	auto MemberExpr = Seq<Token>(memberexpr,
-		expr, 
-		Optional<Token>(NT(),
-			Production<Token>({Token('@'), TYPEID})
-		),
-		Token('.'),
-		OBJECTID,
-		Token('('),
-		Seq<Token>(NT(),
-			expr,
-			Repeat<Token>(NT(), Production<Token>({Token(','), expr}))
-		),
-		Token(')')
+		Production<Token>({Token('~'), NONID_expr}),
+		NONID_expr
 	);
 
 	// clang-format on
 
-	auto g = Combine<Token>(Program, Class, Feature, Formal, Expr, AssignExpr, NotExpr, CompExpr, ArithExpr, MulExpr,
-							IsVoidExpr, NeighExpr, MemberExpr, Literal);
-
+	auto g = Combine<Token>(Program, Class, Feature, Formal, FinishExpr, Expr, AssignExpr, ArithExpr, Term,
+							IsVoidExpr, NeighExpr, FinishTerm, FinishAExpr, CaseLine, NONID_Expr, Init);
+	g.printRules();
+	g.printParseTable();
 	return Parser<Token>(g);
 }
 
